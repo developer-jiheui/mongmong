323e057b37846d6d5ecf01b7ddc5e82669e619d1
jiheui <deveoper.jiheuilee@gmail.com>
2024-05-13 14:32:47 +0900

    thymeleaf configured

---
+++ src/main/java/com/havenwithyou/mongmong/MongmongApplication.java
@@ -0,0 +1,13 @@
+package com.havenwithyou.mongmong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongmongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongmongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/havenwithyou/mongmong/MongmongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.havenwithyou.mongmong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongmongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
